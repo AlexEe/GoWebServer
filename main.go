9bf2252ad3235e7217b8131efa8abfcc0872069b
AlexEe <alexandra.esche@gmx.de>
2019-05-02 17:06:55 +0100

    Add routing to articles

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 )
 
 type Article struct {
+	ID      int    `json:"ID"`
 	Title   string `json:"Title"`
 	Desc    string `json:"desc"`
 	Content string `json:"content"`
@@ -27,6 +28,17 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(articles)
 }
 
+func returnSingleArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	key := vars["key"]
+	var1 := vars["var1"]
+	var2 := vars["var2"]
+
+	fmt.Println("Var 1: " + var1)
+	fmt.Println("Var 2: " + var2)
+	fmt.Fprintf(w, "Key: "+key)
+}
+
 func homePage(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Welcome to my HomePage!")
 	fmt.Println("Endpoint Hit: homePage")
@@ -36,6 +48,7 @@ func handleRequests() {
 	myRouter := mux.NewRouter().StrictSlash(true)
 	myRouter.HandleFunc("/", homePage)
 	myRouter.HandleFunc("/all", returnAllArticles)
+	myRouter.HandleFunc("/article/{key}/{var1}/{var2}/", returnSingleArticle)
 	log.Fatal(http.ListenAndServe(":10000", myRouter))
 }
 
